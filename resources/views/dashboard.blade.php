bb36d8b215ca262324b9c9a4746687bf12ff3476
Reynaldo Velasquez <reynaldoevelasquezjr@gmail.com>
2020-08-20 21:15:10 +0800

    added dashboard page nad css design

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,240 @@
+@extends('layout.app')
+@section('main_content')
+    
+    <div class="reminder-div">
+        <span class="dashboard-title">Dashboard</span>
+        <div class="row">   
+            
+            <div class="colClassroom col-md-6">
+                
+                <div class="reminder">
+                    <span class="reminder-count">
+                        13
+                    </span>
+                    <span class="reminder-text">
+                        Undraged Activity
+                    </span>
+                </div>
+
+                <div class="reminder">
+                    <span class="reminder-count">
+                        3
+                    </span>
+                    <span class="reminder-text">
+                        Ongoing Exams
+                    </span>
+                </div>
+
+                <div class="classroomTable">
+                    <span class="table-title">Classrooms</span>
+                    <table class="classroom-table table table-striped">
+                        <tbody>
+                            <tr>
+                                <td>III - Hope</td>
+                                <td>24 Students</td>
+                            </tr>
+                            <tr>
+                                <td>II - Peace</td>
+                                <td>23 Students</td>
+                            </tr>
+                            <tr>
+                                <td>I - Unity</td>
+                                <td>14 Students</td>
+                            </tr>
+                            <tr>
+                                <td>IV - Bede</td>
+                                <td>34 Students</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                
+                <div class="classroomTable">
+                    <span class="table-title">Classrooms</span>
+                    <table class="classroom-table table table-striped">
+                        <tbody>
+                            <tr>
+                                <td>III - Hope</td>
+                                <td>24 Students</td>
+                            </tr>
+                            <tr>
+                                <td>II - Peace</td>
+                                <td>23 Students</td>
+                            </tr>
+                            <tr>
+                                <td>I - Unity</td>
+                                <td>14 Students</td>
+                            </tr>
+                            <tr>
+                                <td>IV - Bede</td>
+                                <td>34 Students</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                
+                <div class="classroomTable">
+                    <span class="table-title">Classrooms</span>
+                    <table class="classroom-table table table-striped">
+                        <tbody>
+                            <tr>
+                                <td>III - Hope</td>
+                                <td>24 Students</td>
+                            </tr>
+                            <tr>
+                                <td>II - Peace</td>
+                                <td>23 Students</td>
+                            </tr>
+                            <tr>
+                                <td>I - Unity</td>
+                                <td>14 Students</td>
+                            </tr>
+                            <tr>
+                                <td>IV - Bede</td>
+                                <td>34 Students</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+        
+            </div>
+        
+        
+        
+            <div class="colActivities col-md-6">
+             
+                <div class="reminder">
+                    <span class="reminder-count">
+                        2
+                    </span>
+                    <span class="reminder-text">
+                        Ongoing Exams
+                    </span>
+                </div>
+
+                <div class="reminder">
+                    <span class="reminder-count">
+                        5
+                    </span>
+                    <span class="reminder-text">
+                        Ongoing Exams
+                    </span>
+                </div>
+
+                <div class="activityTable">
+                    <span class="table-title">Activities</span>
+
+                    <table class="activity-table table table-striped">
+                        <thead>
+                            <tr>
+                                <th>Title</th>
+                                <th>Classroom</th>
+                            </tr>
+
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Exam 13</td>
+                                <td>III-Fortidue</td>
+                            </tr>
+                             <tr>
+                                <td>Topic 2</td>
+                                <td>III-Fortidue</td>
+                            </tr>
+                             <tr>
+                                <td>Topic 4</td>
+                                <td>III-Fortidue</td>
+                            </tr>
+                             <tr>
+                                <td>Topic 7</td>
+                                <td>III-Fortidue</td>
+                            </tr>
+                             <tr>
+                                <td>Activity 247</td>
+                                <td>III-Fortidue</td>
+                            </tr>
+
+                        </tbody>
+                    </table> 
+                </div>
+                
+                <div class="activityTable">
+                    <span class="table-title">Activities</span>
+
+                    <table class="activity-table table table-striped">
+                        <thead>
+                            <tr>
+                                <th>Title</th>
+                                <th>Classroom</th>
+                            </tr>
+
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Exam 13</td>
+                                <td>III-Fortidue</td>
+                            </tr>
+                             <tr>
+                                <td>Topic 2</td>
+                                <td>III-Fortidue</td>
+                            </tr>
+                             <tr>
+                                <td>Topic 4</td>
+                                <td>III-Fortidue</td>
+                            </tr>
+                             <tr>
+                                <td>Topic 7</td>
+                                <td>III-Fortidue</td>
+                            </tr>
+                             <tr>
+                                <td>Activity 247</td>
+                                <td>III-Fortidue</td>
+                            </tr>
+
+                        </tbody>
+                    </table> 
+                </div>
+                
+                <div class="activityTable">
+                    <span class="table-title">Activities</span>
+
+                    <table class="activity-table table table-striped">
+                        <thead>
+                            <tr>
+                                <th>Title</th>
+                                <th>Classroom</th>
+                            </tr>
+
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Exam 13</td>
+                                <td>III-Fortidue</td>
+                            </tr>
+                             <tr>
+                                <td>Topic 2</td>
+                                <td>III-Fortidue</td>
+                            </tr>
+                             <tr>
+                                <td>Topic 4</td>
+                                <td>III-Fortidue</td>
+                            </tr>
+                             <tr>
+                                <td>Topic 7</td>
+                                <td>III-Fortidue</td>
+                            </tr>
+                             <tr>
+                                <td>Activity 247</td>
+                                <td>III-Fortidue</td>
+                            </tr>
+
+                        </tbody>
+                    </table> 
+                </div>
+     
+                
+            </div>
+        </div>
+    </div>
+    
+@endsection
